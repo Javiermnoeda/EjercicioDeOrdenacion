f1e7b0cbe396c9f5dd0009fbed46652e114578aa
Javiermnoeda <jmnoeda2000@gmail.com>
2021-05-05 16:55:17 +0200

    Ejercicio Terminado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,7 +6,7 @@ class AlumnoComparable(var Nombre : String, var Nota : Int) : Comparable<AlumnoC
     }
 
     override fun toString(): String {
-        return "$Nombre y ha sacado un $Nota de nota"
+        return "$Nombre y ha sacado un $Nota"
     }
 }
 
@@ -17,7 +17,6 @@ fun main() {
     //PARTE 1
 
     val listaNumerosAleatorios = mutableListOf<Int>()
-
     var numero = 0
 
     repeat(100){
@@ -28,24 +27,35 @@ fun main() {
     println("La lista ordenada de menor a mayor es: ${listaNumerosAleatorios.sorted()}")
     println("La lista ordenada de mayor a menor es: ${listaNumerosAleatorios.sortedDescending()}")
 
-    val listaNumerosMenorMayor: MutableList<Int> = listaNumerosAleatorios.sorted() as MutableList<Int>
-    val listaNumerosMayorMenor: MutableList<Int> = listaNumerosAleatorios.sortedDescending() as MutableList<Int>
+    val listaNumerosMenorMayor = listaNumerosAleatorios.sorted()
+    val listaNumerosMayorMenor = listaNumerosAleatorios.sortedDescending()
 
     println("La lista de los numeros ordenados de menor a mayor es: $listaNumerosMenorMayor")
-    println("La lista de los numeros ordenados de menor a mayor es: $listaNumerosMayorMenor")
+    println("La lista de los numeros ordenados de mayor a menor es: $listaNumerosMayorMenor")
 
 
     //PARTE 2
-
     repeat(100){
-        val alumno = AlumnoComparable("Alumno $it", Nota = Random.nextInt(0,10))
+        val alumno = AlumnoComparable("Alumno ${it+1}", Nota = Random.nextInt(0,11))
         listaAlumnos.add(alumno)
     }
 
+    //Ordenados por Nota
+    println("\nLos alumnos ordenados por nota son")
+
     listaAlumnos.sort()
-    println("Los alumnos ordenados por nota son: $listaAlumnos")
+    listaAlumnos.forEach { println(it.toString()) }
+
+    //Ordenados por Longitud de nombre
+    println("\nLos alumnos ordenados por longitud de nombre son")
 
     listaAlumnos.sortByDescending { it.Nombre.length }
-    println("Los alumnos ordenados por longitud de nombre son: $listaAlumnos")
+    listaAlumnos.forEach { println(it.toString()) }
+
+    //Ordenados alfabéticamente
+    println("\nLos alumnos ordenados alfabéticamente")
+
+    listaAlumnos.sortBy {it.Nombre}
+    listaAlumnos.forEach { println(it.toString()) }
 
 }
